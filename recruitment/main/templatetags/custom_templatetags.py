eccc9cb9ee0cc7bd77f491f28c31a5bfcdbfab71
Wan Fakhrurrazi <wanfakhrulwanazizan15@gmail.com>
2023-05-11 17:28:08 +0800

    Commit 11-5-23 - Added hiring manager screening & final decision section - added status_editable_field.html & ~.js

--- recruitment/main/templatetags/custom_templatetags.py
+++ recruitment/main/templatetags/custom_templatetags.py
@@ -29,3 +29,26 @@ def show_eval_lead(lead):
 @register.inclusion_tag("main/components/initscreening/label_leads.html")
 def show_pending_lead(lead):    
     return {"lead": lead,}
+
+@register.inclusion_tag("main/components/initscreening/status_editable_field.html")
+def show_status_editable_field(
+    is_proceed:bool, 
+    label = None, 
+    proceed_label = "proceed", 
+    reject_label = "reject", 
+    attr_name = "status-edit",
+    show_submit_btn = False,
+    submit_name = "is_proceed",
+):
+    
+    return {
+        "label": label,
+        "proceed_label": proceed_label,
+        "reject_label": reject_label,
+        "is_proceed": is_proceed,
+        "attr_name": attr_name,
+        "show_submit_btn": show_submit_btn,
+        "submit_name": submit_name,
+    }
+
+

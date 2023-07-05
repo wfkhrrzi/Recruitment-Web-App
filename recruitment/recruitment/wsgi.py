08941fb4af31d1b545d721802bfb25d0d9eaec7a
Wan Fakhrurrazi <wanfakhrulwanazizan15@gmail.com>
2023-07-05 16:34:10 +0800

    added os.chdir

--- recruitment/recruitment/wsgi.py
+++ recruitment/recruitment/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recruitment.settings')

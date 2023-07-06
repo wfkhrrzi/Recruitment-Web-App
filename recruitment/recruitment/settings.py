a8abec546338fe5cd9a70768ee87db11b2b5b286
Wan Fakhrurrazi <wanfakhrulwanazizan15@gmail.com>
2023-07-06 12:10:56 +0800

    added whitenoise to installed_apps

--- recruitment/recruitment/settings.py
+++ recruitment/recruitment/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     # 'channels',
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

2a8a7eb28a9c89b4638e4de8af962107e1b3a825
Wan Fakhrurrazi <wanfakhrulwanazizan15@gmail.com>
2023-07-06 11:07:34 +0800

    modified manage.py

--- recruitment/manage.py
+++ recruitment/manage.py
@@ -15,8 +15,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(['manage.py', 'runserver'])
-
+    
+    if len(sys.argv) > 1:
+        execute_from_command_line(sys.argv)
+    else:
+        execute_from_command_line(['manage.py','runserver'])
 
 if __name__ == '__main__':
     main()

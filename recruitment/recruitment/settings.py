db9d22a1bd4d28faf81dc442ebc37d78a88a87dc
wfkhrrzi <wanfakhrulwanazizan15@gmail.com>
2023-07-28 15:33:25 +0800

    added comment dbsqlite

--- recruitment/recruitment/settings.py
+++ recruitment/recruitment/settings.py
@@ -118,6 +118,7 @@ DATABASES = {
 
 # Celery settings
 CELERY_BROKER_URL = f"sqla+postgresql://{str(DATABASES['default']['USER'])}:{urllib.parse.quote(str(DATABASES['default']['PASSWORD']), safe='')}@{str(DATABASES['default']['HOST'])}:{str(DATABASES['default']['PORT'])}/{str(DATABASES['default']['NAME'])}"
+# CELERY_BROKER_URL = 'sqla+sqlite:///' + str(DATABASES['default']['NAME'])
 # CELERY_RESULT_BACKEND = 'db+sqlite:///' + str(DATABASES['default']['NAME'])
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_RESULT_EXTENDED = True

2d36d7c4b19183186156ca231066854e4c7b72bd
wfkhrrzi <wanfakhrulwanazizan15@gmail.com>
2023-08-09 19:55:03 +0800

    remove load_dotenv

--- recruitment/manage.py
+++ recruitment/manage.py
@@ -2,11 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
-    load_dotenv()   
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recruitment.settings')
     try:
         from django.core.management import execute_from_command_line
